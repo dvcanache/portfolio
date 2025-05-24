3fcb2ea54baa613a569fbd5b0b446db4736f1e40
Fernando <frr4212@gmail.com>
2025-05-24 04:18:08 -0400

    2 proyectos agregados

--- data/content/projects.ts
+++ data/content/projects.ts
@@ -4,16 +4,34 @@ import { Project } from "types";
 const projects: Project[] = [
   {
     id: 0,
-
     title: "EverestGym",
     desc: "Un proyecto para un gimnasio local",
     img: "/static/projects/Everest.png",
-    link: "https://dvcanache.github.io/EverestGym/",
+    link: "https://everestgym.netlify.app/",
     github: "https://github.com/dvcanache/EverestGym",
     tags: ["React", "TailwindCSS", "CSS"],
   }, 
+
   {
     id: 1,
+    title: "CodeLAN",
+    desc: "Pagina de retos de programación",
+    img: "/static/projects/CodeLan.png",
+    link: "https://codelan.netlify.app/",
+    github: "https://github.com/dvcanache/UnNuevoDia",
+    tags: ["NextJS", "TailwindCSS", "CSS","Shadcn/ui" ],
+  },
+  {
+    id: 2,
+    title: "Un Nuevo Dia",
+    desc: "Fundacion benefica",
+    img: "/static/projects/UnNuevoDia.png",
+    link: "https://unnuevodia.netlify.app/",
+    github: "https://github.com/dvcanache/UnNuevoDia",
+    tags: ["NextJS", "TailwindCSS", "CSS"],
+  },
+  {
+    id: 3,
     title: "Dd Portafolio",
     desc: "Portafolio personal",
     img: "/static/projects/DdPortafolio.png",
@@ -21,15 +39,17 @@ const projects: Project[] = [
     github: "https://github.com/dvcanache/DdPortafolio",
     tags: ["NextJS", "TailwindCSS", "CSS"],
   },
-  {
-    id: 2,
-    title: "UnNuevoDia",
-    desc: "Fundacion benefica",
-    img: "/static/projects/UnNuevoDia.png",
+  
+   {
+    id: 4,
+    title: "SweetHealth",
+    desc: "Aplicacion Móvil para dieta de personas con diabetes o cardiopatía",
+    img: "/static/projects/SweetHealth.png",
     link: "https://unnuevodia.netlify.app/",
     github: "https://github.com/dvcanache/UnNuevoDia",
-    tags: ["NextJS", "TailwindCSS", "CSS"],
+    tags: ["Flutter", "Firebase", "Figma"],
   },
+  
 ];
 
 export const allTags = []
